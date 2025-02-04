30cf2e440300f85d6de7193e3dce79c3dc119414
Anna Tereshonok <ananatereshonok@yandex.ru>
2025-02-04 03:36:06 +0300

    Added tests for widger/get_date

--- src/widget.py
+++ src/widget.py
@@ -6,28 +6,28 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(account: str) -> str:
     """
     Маскирует номер кредитной карты/счета, отображая его в формате:
-    Для карт: [Название карты] XXXX XX** **** XXXX, где X — это цифра номера
-    Для счетов: Счет **XXXX, где X — это цифра номера.
+        Для карт: [Название карты] XXXX XX** **** XXXX, где X — это цифра номера
+        Для счетов: Счет **XXXX, где X — это цифра номера.
 
     Параметры:
-    account (str): Номер карты/счета в строковом формате.
+        account (str): Номер карты/счета в строковом формате.
 
     Возвращает:
-    str: Замаскированный номер карты/счета.
+        str: Замаскированный номер карты/счета.
     """
-    # ("Cчет", "Номер счета не может быть пустым"),
+
     if not account or account.strip() == "":
         raise ValueError("Не введен номер карты/счета")
 
     if account.isdigit():
         raise ValueError("Данные введены некорректно")
 
-    account_splitted = [item.strip() for item in account.split()]
-    account_name_list = [item for item in account_splitted if item.isalpha()]
-    account_name = ' '.join(account_name_list)
-    account_number = ' '.join([item for item in account_splitted if item not in account_name_list]) or ""
+    account_split = [item.strip() for item in account.split()]
+    account_name_list = [item for item in account_split if item.isalpha()]
+    account_name = " ".join(account_name_list)
+    account_number = " ".join([item for item in account_split if item not in account_name_list]) or ""
 
-    if account_name == 'Счет':
+    if account_name == "Счет":
         masked_account = get_mask_account(account_number)
     else:
         masked_account = get_mask_card_number(account_number)
@@ -37,7 +37,28 @@ def mask_account_card(account: str) -> str:
 
 
 def get_date(date: str) -> str:
-    date_object = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
-    formatted_date = date_object.strftime("%d.%m.%Y")
-    return formatted_date
+    """
+    Преобразует дату из формата ISO 8601 ("YYYY-MM-DDTHH:MM:SS.ssssss") в формат "DD.MM.YYYY".
 
+    Параметры:
+        date (str): Дата в формате "YYYY-MM-DDTHH:MM:SS.ssssss".
+
+    Возвращает:
+        str: Дата в формате "DD.MM.YYYY".
+
+    Raises:
+        ValueError: Если передано не строковое значение.
+        ValueError: Если строка пустая или содержит только пробелы.
+        ValueError: Если дата не соответствует формату "YYYY-MM-DDTHH:MM:SS.ssssss".
+    """
+    if not isinstance(date, str):
+        raise ValueError("Дата должна быть строкой")
+
+    if not date.strip():
+        raise ValueError("Дата не может быть пустой")
+
+    try:
+        date_object = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        return date_object.strftime("%d.%m.%Y")
+    except ValueError:
+        raise ValueError("Некорректный формат даты")
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 
 def test_mask_account_card_positive(params_for_widget_mask_positive):
@@ -12,4 +12,16 @@ def test_mask_account_card_negative(params_for_widget_mask_negative):
     card_number, expected_error = params_for_widget_mask_negative
     with pytest.raises(ValueError) as exc_info:
         mask_account_card(card_number)
+    assert str(exc_info.value) == expected_error
+
+
+def test_get_date_positive(params_for_widget_date_positive):
+    date, expected_results = params_for_widget_date_positive
+    assert get_date(date) == expected_results
+
+
+def test_get_date_negative(params_for_widget_date_negative):
+    date, expected_error = params_for_widget_date_negative
+    with pytest.raises(ValueError) as exc_info:
+        get_date(date)
     assert str(exc_info.value) == expected_error
